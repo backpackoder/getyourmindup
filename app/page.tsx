46100b1b32d60b93d5841910a768ab1d08669a85
harderth <th.barbiera@outlook.fr>
2023-07-14 15:14:05 -0600

    Adding header and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Get your mind up</h1>
-    </main>
-  );
+  return <h1 className="text-4xl font-bold">Get your mind up</h1>;
 }
